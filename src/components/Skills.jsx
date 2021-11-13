6d0afc7a8e49babcf2c1626cd136a30bae6ab554
natsgt <nataliardodas7858@gmail.com>
2021-11-14 00:23:51 +0100

    changed content and styling

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,38 +1,41 @@
-import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import data from "../cv/Cv";
-import { TitleSection } from "./Titles";
+import React from "react"
+import Jumbotron from "react-bootstrap/Jumbotron"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import data from "../cv/Cv"
+import { TitleSection } from "./Titles"
 
-library.add(fab);
+library.add(fab)
 
 function Skills() {
   return (
-    <Jumbotron id="skills" fluid className="section-container primary-color-section d-flex flex-column justify-content-center">
-      <Container className="main-container d-flex flex-column justify-content-center">
-        <TitleSection title="Skills" classElement="section-title" />
-        <p className="skills-text">
-          {data.skillsDescription}
-        </p>
-        <div className="d-flex justify-content-center mt-5">
-          <Row xs={2} className="mx-5">
+    <Jumbotron
+      id='skills'
+      fluid
+      className='section-container primary-color-section d-flex flex-column justify-content-center'
+    >
+      <Container className='main-container d-flex flex-column justify-content-center'>
+        <TitleSection title='Skills' classElement='section-title' />
+        <p className='skills-text'>{data.skillsDescription}</p>
+        <div className='d-flex justify-content-center mt-5'>
+          <Row xs={2} className='mx-5'>
             {data.skills.map((skill) => {
               return (
-                <Col key={skill.id} lg="4">
-                  <FontAwesomeIcon icon={skill.icon} /> {skill.name}
+                <Col key={skill.id} lg='4' className='one-skill-item'>
+                  <FontAwesomeIcon icon={skill.icon} />{" "}
+                  <span>{skill.name}</span>
                 </Col>
               )
             })}
           </Row>
         </div>
       </Container>
-    </Jumbotron >
-  );
+    </Jumbotron>
+  )
 }
 
-export default Skills;
+export default Skills
--- src/cv/Cv.jsx
+++ src/cv/Cv.jsx
@@ -1,5 +1,5 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faDatabase, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faDatabase, faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import {
   fab,
   faHtml5,
@@ -9,18 +9,22 @@ import {
   faNodeJs,
   faReact,
   faLinkedin,
-  faGithub
-} from "@fortawesome/free-brands-svg-icons";
-library.add(fab);
+  faGithub,
+  faSass
+} from "@fortawesome/free-brands-svg-icons"
+library.add(fab)
 
 const data = {
   name: "Natalia Rodas",
   location: "Sydhavn, Copenhaguen, Denmark",
-  briefDescription: "I’m a Front End Developer based in Copenhaguen, Denmark. I’m passionate about web development, technology, and coding. I have a slight obsession with logic and challenges. ",
+  briefDescription:
+    "I’m a Front End Developer based in Copenhaguen, Denmark. I’m passionate about web development, technology, and coding. I have a slight obsession with logic and challenges. ",
   aboutMe:
-    "I started my career in construction where I worked with wonderful people, lead amazing projects, and also worked independently, always looking forward to achieve goals. When I moved to Denmark in 2019, I switched my career to web developer where I started learning the fundamentals of programming with Java. Afterward, I studied HTML and CSS on online platforms such as Coursera, Udemy, and FreeCode Camp. I'm currently on my way to get my Full Stack Developer certification from Hack Your Future.",
-  skillsDescription: "I started as a self-taught developer on several online platforms and trying to build some basic projects (cloning responsive websites). Afterward, I got into the Hack Your Future program where I'm currently studying for Full Stack Developer. Here I've acquired the knowledge to build other projects more complex with Javascript, NodeJs, React, MySql, KnexJs, and Git. I'm always looking for new projects that help me dig more into these technologies and I'm looking forward to new and challenges.",
-  contactSummary: "Hey! you just met me, and this is crazy, so here's my contact, so write me... maybe?",
+    "I’m a Front End Developer mainly interested in getting more experience in coding. I like playing with CSS (weirdo, I know) and also like to play around with designs on Figma. But also I have an obsession with coding challenges. That’s how I knew this was career was for me, I get to combine my creative side with my logical side, with a daily dose of new challenges.",
+  skillsDescription:
+    "I started as a self-taught developer on several online platforms and built some basic projects (cloning responsive websites). Afterward, I got into the Hack Your Future program where I'm currently got my Front End Developer certificate. Since then, I’ve been working with projects based on Javascript, NodeJs, React, MySql, KnexJs, Git, Sass, Redux, and other several libraries. I'm always looking for new projects that help me dig more into these technologies and I'm looking forward to new and challenges.",
+  contactSummary:
+    "Hey! you just met me, and this is crazy, so here's my contact, so write me... maybe?",
   contact: [
     {
       id: 1,
@@ -50,47 +54,48 @@ const data = {
       name: "Html",
       icon: faHtml5
     },
-    { name: "Css", icon: faCss3 },
+    { id: 2, name: "Css", icon: faCss3 },
+    { id: 3, name: "Sass", icon: faSass },
     {
-      id: 2,
+      id: 4,
       name: "Bootstrap",
       icon: faBootstrap
     },
     {
-      id: 3,
+      id: 5,
       name: "Javascript (ES6)",
       icon: faJs
     },
     {
-      id: 4,
+      id: 6,
       name: "NodeJs",
       icon: faNodeJs
     },
     {
-      id: 5,
+      id: 7,
       name: "MySql",
       icon: faDatabase
     },
     {
-      id: 6,
+      id: 8,
       name: "React",
       icon: faReact
     },
     {
-      id: 7,
+      id: 9,
       name: "GitHub",
       icon: faGithub
     }
   ],
-  projectsSummary: "These are some of the projects that I have been working on. Click on the card to see the code behind it or hover over it to see a giph showing how the website looks.",
+  projectsSummary:
+    "These are some of the projects that I have been working on. Click on the card to see the code behind it or hover over it to see a giph showing how the website looks.",
   projects: [
     {
       id: 1,
       title: "Clone Website",
       description:
         "This project was based on pure HTML and CSS. It's a responsive website with diverse image positioning, buttons, a form, and other components that are styled without any library. The design and original template can be found here.",
-      img:
-        "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
       code: "https://github.com/NatsGt/clone-travel-app",
       preview: "https://natsgt.github.io/clone-travel-app/"
     },
@@ -99,8 +104,7 @@ const data = {
       title: "GitHub Search",
       description:
         "This is a pretty basic search website for GitHub users based on React. It fetches data from a GitHub API, passes it through components using React hooks, and handles errors that the API may throw.",
-      img:
-        "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
       code: "https://github.com/NatsGt/hyf-homework/tree/master/react/week4",
       preview: "https://i.imgur.com/1A8ZJqI.gif"
     },
@@ -109,14 +113,13 @@ const data = {
       title: "MealSharing App",
       description:
         "This project consists on the use of several tools, such as React, NodeJs, MySql, KnexJs, and Bootstrap. It's a project that uses CRUD concepts, fetching information from a database, and using React-Bootstrap for the styling.",
-      img:
-        "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
       code: "https://github.com/NatsGt/meal-sharing",
       preview: "https://meal-sharing-natsgt.herokuapp.com/"
     }
   ]
-};
-export default data;
+}
+export default data
 
 /*
 https://www.freecodecamp.org/news/coding-projects-to-include-in-your-frontend-portfolio/
